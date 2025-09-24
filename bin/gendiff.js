3954a4b67ce7d7c329fe3f55a0bf5e1d84cc913f
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-09-24 19:37:46 +0300

    modify a gendiff.js and specify the entry point to the package in package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from "commander"
+import app from '../src/index.js'
 
 const program = new Command()
 
-program
+const gendiff = program
 .name('gendiff')
 .description('Compares two configuration files and shows a difference.')
 .version('1.0.0')
 .option('-f, --format [type]', 'output format')
 .argument('<filepath1>')
 .argument('<filepath2>')
+.action((a, b, options) => {
+    app(a, b)
+})
 
 program.parse()
