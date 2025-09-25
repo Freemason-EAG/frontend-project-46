f53a08d91471c8bd8f42944c4cd26336a404b46a
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-09-25 21:48:53 +0300

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import app from '../src/index.js'
 
 const program = new Command()
 
-const gendiff = program
+program
 .name('gendiff')
 .description('Compares two configuration files and shows a difference.')
 .version('1.0.0')
