3c0d1fff0e997b7b90989539333a188b7e69ee17
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-09-26 22:54:08 +0300

    Add the __dirname function to tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,10 +1,14 @@
 import genDiff from '../src/index.js'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 let firstJsonFile
 let secondJsonFile
 beforeEach(() => {
-  firstJsonFile = '__fixtures__/file1.json'
-  secondJsonFile = '__fixtures__/file2.json'
+  firstJsonFile = path.join(__dirname, '..', '__fixtures__/file1.json')
+  secondJsonFile = path.join(__dirname, '..', '__fixtures__/file2.json')
 })
 
 test('genDiff', () => {
@@ -25,5 +29,6 @@ test('type of result to be string', () => {
 })
 
 test('transferred an empty file', () => {
-  expect(() => genDiff('__fixtures__/emptyFile.json', secondJsonFile)).toThrow('empty file transferred')
+  const emptyFile = path.join(__dirname, '..', '__fixtures__/emptyFile.json')
+  expect(() => genDiff(emptyFile, secondJsonFile)).toThrow('empty file transferred')
 })
