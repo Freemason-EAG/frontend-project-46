3b8918af96dc98838aca0679ea5e8015fe2f3086
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-09-28 17:39:47 +0300

    add fixtures and tests for yaml-format

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,16 +4,17 @@ import path from 'path'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-let firstJsonFile
-let secondJsonFile
-beforeEach(() => {
-  firstJsonFile = path.join(__dirname, '..', '__fixtures__/file1.json')
-  secondJsonFile = path.join(__dirname, '..', '__fixtures__/file2.json')
-})
+describe('genDiff for JSON', () => {
+  let firstJsonFile
+  let secondJsonFile
+  beforeEach(() => {
+    firstJsonFile = path.join(__dirname, '..', '__fixtures__/file1.json')
+    secondJsonFile = path.join(__dirname, '..', '__fixtures__/file2.json')
+  })
 
-test('genDiff', () => {
-  expect(genDiff(firstJsonFile, secondJsonFile)).toEqual(
-    `{
+  test('genDiff', () => {
+    expect(genDiff(firstJsonFile, secondJsonFile)).toEqual(
+      `{
 - follow: false
   host: hexlet.io
 - proxy: 123.234.53.22
@@ -21,14 +22,46 @@ test('genDiff', () => {
 + timeout: 20
 + verbose: true
 }`,
-  )
+    )
+  })
+
+  test('type of result to be string', () => {
+    expect(typeof (genDiff(firstJsonFile, secondJsonFile))).toBe('string')
+  })
+
+  test('transferred an empty file', () => {
+    const emptyFile = path.join(__dirname, '..', '__fixtures__/emptyFile.json')
+    expect(() => genDiff(emptyFile, secondJsonFile)).toThrow('empty file transferred')
+  })
 })
 
-test('type of result to be string', () => {
-  expect(typeof (genDiff(firstJsonFile, secondJsonFile))).toBe('string')
-})
+describe('genDiff for YML', () => {
+  let firstYmlFile
+  let secondYmlFile
+  beforeEach(() => {
+    firstYmlFile = path.join(__dirname, '..', '__fixtures__/file1.yml')
+    secondYmlFile = path.join(__dirname, '..', '__fixtures__/file2.yml')
+  })
 
-test('transferred an empty file', () => {
-  const emptyFile = path.join(__dirname, '..', '__fixtures__/emptyFile.json')
-  expect(() => genDiff(emptyFile, secondJsonFile)).toThrow('empty file transferred')
+  test('genDiff', () => {
+    expect(genDiff(firstYmlFile, secondYmlFile)).toEqual(
+      `{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`,
+    )
+  })
+
+  test('type of result to be string', () => {
+    expect(typeof (genDiff(firstYmlFile, secondYmlFile))).toBe('string')
+  })
+
+  test('transferred an empty file', () => {
+    const emptyFile = path.join(__dirname, '..', '__fixtures__/emptyFile.yml')
+    expect(() => genDiff(emptyFile, secondYmlFile)).toThrow('empty file transferred')
+  })
 })
