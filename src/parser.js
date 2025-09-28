688e19443cac67b6d17e109aae7b84751608cb32
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-09-28 17:56:57 +0300

    install js-yaml and add YAML support in parser

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,10 @@
 import path from 'path'
+import yaml from 'js-yaml'
 
 export default (data, filename) => {
   const format = path.extname(filename).toLowerCase()
   if (format === '.json') return JSON.parse(data)
-  if (format === '.yaml' || format === '.yml') throw new Error('the format is not processed yet')
+  if (format === '.yaml' || format === '.yml') return yaml.load(data)
   else {
     throw new Error(`${format} is wrong format`)
   }
