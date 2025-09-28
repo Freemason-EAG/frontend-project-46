2a69d0fed0d9ca1ef51b05317c28e14d52ca3c39
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-09-28 16:54:59 +0300

    Fix index.js logic

--- src/index.js
+++ src/index.js
@@ -3,33 +3,37 @@ import fs from 'fs'
 import _ from 'lodash'
 import parser from './parser.js'
 
-const genDiff = (filepath1, filepath2) => {
-  const path1 = path.resolve(filepath1)
-  const path2 = path.resolve(filepath2)
-  let content1
-  let content2
-  try {
-    content1 = parser(fs.readFileSync(path1, 'utf-8'), filepath1)
-    content2 = parser(fs.readFileSync(path2, 'utf-8'), filepath2)
-  }
-  catch {
-    throw new Error('empty file transferred')
-  }
-
-  const bothContentKeys = _.sortBy(_.union(Object.keys(content1), Object.keys(content2)))
-
-  const structured = bothContentKeys.reduce((acc, key) => {
-    if (Object.hasOwn(content1, key) && Object.hasOwn(content2, key)) {
-      content1[key] === content2[key] ? acc.push(`  ${key}: ${content1[key]}`) : acc.push(`- ${key}: ${content1[key]}\n+ ${key}: ${content2[key]}`)
+const formatter = (obj1, obj2, arrKeys) => {
+  return arrKeys.reduce((acc, key) => {
+    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+      obj1[key] === obj2[key] ? acc.push(`  ${key}: ${obj1[key]}`) : acc.push(`- ${key}: ${obj1[key]}\n+ ${key}: ${obj2[key]}`)
     }
-    else if (Object.hasOwn(content1, key) && !Object.hasOwn(content2, key)) {
-      acc.push(`- ${key}: ${content1[key]}`)
+    else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+      acc.push(`- ${key}: ${obj1[key]}`)
     }
-    else if (Object.hasOwn(content2, key) && !Object.hasOwn(content1, key)) {
-      acc.push(`+ ${key}: ${content2[key]}`)
+    else if (Object.hasOwn(obj2, key) && !Object.hasOwn(obj1, key)) {
+      acc.push(`+ ${key}: ${obj2[key]}`)
     }
     return acc
   }, [])
+}
+
+const genDiff = (filepath1, filepath2) => {
+  const path1 = path.resolve(filepath1)
+  const path2 = path.resolve(filepath2)
+
+  const data1 = fs.readFileSync(path1, 'utf-8')
+  const data2 = fs.readFileSync(path2, 'utf-8')
+
+  if (data1.trim().length === 0 || data2.trim().length === 0) {
+    throw new Error('empty file transferred')
+  }
+  const content1 = parser(data1, filepath1)
+  const content2 = parser(data2, filepath2)
+
+  const bothContentKeys = _.sortBy(_.union(Object.keys(content1), Object.keys(content2)))
+
+  const structured = formatter(content1, content2, bothContentKeys)
   return `{\n${structured.join('\n')}\n}`
 }
 
