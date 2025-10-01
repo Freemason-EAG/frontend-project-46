4c8f0b51055335dca6e98f285ee08af73855d470
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-10-01 19:04:12 +0300

    create plain.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,13 +10,13 @@ const gendiff = program
 .name('gendiff')
 .description('Compares two configuration files and shows a difference.')
 .version('1.0.0')
-.option('-f, --format [type]', 'output format')
+.option('-f, --format [type]', 'output format', 'stylish')
 .argument('<filepath1>')
 .argument('<filepath2>')
 .action((a, b, options) => {
     let format
     if (options.format) {
-        format = option.format
+        format = options.format
     }
     format = stylish
     console.log(genDiff(a, b, format))
