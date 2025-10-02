9ba6fa59cee6c67bfa94f400b5464b3e9487a868
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-10-02 18:00:05 +0300

    add tests for plain format

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -71,7 +71,7 @@ describe('genDiff: JSON nested structure test', () => {
   })
 })
 
-describe('genDiff: JSON nested structure test', () => {
+describe('genDiff: YAML nested structure test', () => {
   let firstYmlFile
   let secondYmlFile
   beforeEach(() => {
@@ -135,5 +135,39 @@ describe('genDiff: JSON nested structure test', () => {
   test('transferred an empty file', () => {
     const emptyFile = path.join(__dirname, '..', '__fixtures__/emptyFile.yml')
     expect(() => genDiff(emptyFile, secondYmlFile)).toThrow('empty file transferred')
+  })
+})
+
+describe('genDiff: Plain format check', () => {
+  let firstJsonFile
+  let secondJsonFile
+  beforeEach(() => {
+    firstJsonFile = path.join(__dirname, '..', '__fixtures__/file1.json')
+    secondJsonFile = path.join(__dirname, '..', '__fixtures__/file2.json')
+  })
+
+  test('genDiff', () => {
+    expect(genDiff(firstJsonFile, secondJsonFile, 'plain')).toEqual(
+      `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`,
+    )
+  })
+
+  test('type of result to be string', () => {
+    expect(typeof (genDiff(firstJsonFile, secondJsonFile))).toBe('string')
+  })
+
+  test('transferred an empty file', () => {
+    const emptyFile = path.join(__dirname, '..', '__fixtures__/emptyFile.json')
+    expect(() => genDiff(emptyFile, secondJsonFile)).toThrow('empty file transferred')
   })
 })
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,21 +4,20 @@ import { Command } from "commander"
 import genDiff from '../src/index.js'
 import stylish from "../src/formatters/stylish.js"
 
+const formats = { stylish }
+
 const program = new Command()
 
-const gendiff = program
+program
 .name('gendiff')
 .description('Compares two configuration files and shows a difference.')
 .version('1.0.0')
 .option('-f, --format [type]', 'output format', 'stylish')
 .argument('<filepath1>')
 .argument('<filepath2>')
-.action((a, b, options) => {
-    let format
-    if (options.format) {
-        format = options.format
-    }
-    format = stylish
+.action((a, b) => {
+    const selectedFormat = program.opts().format || 'stylish'
+    const format = formats[selectedFormat] || stylish
     console.log(genDiff(a, b, format))
 })
 
