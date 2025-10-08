c8052fa23d36ae6f43e6a6af776b5844e49b83ac
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-10-09 00:42:53 +0600

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -13,8 +13,7 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, opts) => {
-    const selectedFormat = opts.format || 'stylish'
-    console.log(genDiff(filepath1, filepath2, selectedFormat))
+    console.log(genDiff(filepath1, filepath2, opts.format || 'stylish'))
   })
 
 program.parse()
