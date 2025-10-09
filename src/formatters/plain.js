888bdfa42d4ec52bd1d964f88e3dcf280ed79f93
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-10-09 13:09:42 +0600

    rewrite plain using switch and remove explicit filter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -13,25 +13,22 @@ const getTrueValue = (value) => {
 
 const plain = (arrOfObjs) => {
   const iter = (objs, parents = '') => {
-    const transformToPlain = objs.flatMap((obj) => {
-      if (obj.status === 'added') {
+    return objs
+      .flatMap((obj) => {
         const newParents = buildPropertyPath(parents, obj.key)
-        return `Property '${newParents}' was added with value: ${getTrueValue(obj.value)}`
-      }
-      if (obj.status === 'deleted') {
-        const newParents = buildPropertyPath(parents, obj.key)
-        return `Property '${newParents}' was removed`
-      }
-      if (obj.status === 'changed') {
-        const newParents = buildPropertyPath(parents, obj.key)
-        return `Property '${newParents}' was updated. From ${getTrueValue(obj.value1)} to ${getTrueValue(obj.value2)}`
-      }
-      if (obj.status === 'recursion') {
-        const newParents = buildPropertyPath(parents, obj.key)
-        return iter(obj.children, newParents)
-      }
-    })
-    return transformToPlain.filter(Boolean) // убираем undefined
+        switch (obj.status) {
+          case 'added':
+            return `Property '${newParents}' was added with value: ${getTrueValue(obj.value)}`
+          case 'deleted':
+            return `Property '${newParents}' was removed`
+          case 'changed':
+            return `Property '${newParents}' was updated. From ${getTrueValue(obj.value1)} to ${getTrueValue(obj.value2)}`
+          case 'recursion':
+            return iter(obj.children, newParents)
+          default:
+            return []
+        }
+      })
   }
   return `${iter(arrOfObjs, '').join('\n')}`
 }
